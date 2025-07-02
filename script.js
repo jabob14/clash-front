fe235e8034ca82a39ed32d0a772f4e3bd8c24a07
Jakob Gerell <jakob@gerell.se>
2025-07-02 21:25:51 +0200

    Added font

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ fetch('members.json')
     const membersContainer = document.getElementById("members");
     let rank = 1;
 
+    const rankColors = {
+      1: "#f8d65a",
+      2: "#d1e0e9",
+      3: "#e8b13f"
+    };
+
     for (const memberId in data.memberList) {
       const member = data.memberList[memberId];
       const row = document.createElement("div");
       row.className = "row";
+
+      if (rankColors[rank]) {
+        row.style.backgroundColor = rankColors[rank];
+      }
+
       row.innerHTML = `
         <div class="rank">${rank}</div>
         <div class="name">${member.name}</div>
+        <div class="donations">${member.donations}</div>
+        <div class="capital">${member.capitalAttacks}</div>
+        <div class="games">${member.clanGamesScore}</div>
+        <div class="stars">${member.clanWarLeagueStars}</div>
+        <div class="score">${member.totalScore}</div>
       `;
       membersContainer.appendChild(row);
       rank++;
